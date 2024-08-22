4aaa5ae4c60aa37b22f9a01da57f296f51928909
Chetan Singh <chet4nsingh@gmail.com>
2024-08-22 10:16:52 +0530

    React + Tailwind CSS

---
+++ src/components/LandingPage/Header.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "../../assets/react.svg";
+
+export default function Header() {
+  return (
+    <header className="w-full p-4 border text-white flex justify-between items-center">
+      <img id="react-logo" src={reactLogo} alt="React" className="w-16" />
+      <h1 className="text-4xl">React Landing Page</h1>
+      <nav>
+        <button className="bg-sky-700 px-4 py-1 rounded-md mr-4">
+          Register
+        </button>
+        <button className="bg-sky-700 px-4 py-1 rounded-md">Login</button>
+      </nav>
+    </header>
+  );
+}
