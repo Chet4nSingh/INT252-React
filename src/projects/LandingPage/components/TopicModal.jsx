b86aae74fa13a4d57ec064d89c0724954289abb3
Chetan Singh <chet4nsingh@gmail.com>
2024-08-27 22:02:20 +0530

    React + Tailwind CSS

--- src/projects/LandingPage/components/TopicModal.jsx
+++ src/projects/LandingPage/components/TopicModal.jsx
@@ -1,6 +1,7 @@
 export default function TopicModal({ topic }) {
   return (
     <dialog className="w-3/5 h-4/5 p-8 bg-slate-800 border text-white" open>
+      <h1 className="text-6xl">{topic.name}</h1>
     </dialog>
   );
 }
