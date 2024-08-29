4bc139ec2af9a6aa644af54cfcc9b434a4446084
Chetan Singh <chet4nsingh@gmail.com>
2024-08-29 10:47:57 +0530

    React + Tailwind CSS

--- src/projects/AddToCart/components/Products.jsx
+++ src/projects/AddToCart/components/Products.jsx
@@ -4,9 +4,13 @@ export default function Products() {
   return (
     <ul className="w-[90%] mx-auto grid grid-cols-3 gap-8">
       {FASHION_PRODUCTS.map((product) => (
-        <li key={product.id} className="text-white border relative">
+        <li key={product.id} className="text-white border relative group">
           <div className="h-full">
             <img className="h-full" src={product.image} alt="fashion product" />
+          </div>
+          <div className="w-full p-4 absolute bottom-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 translate-y-8 group-hover:translate-y-0 transition-all duration-300 ">
+            <h2 className="text-2xl">{product.name}</h2>
+            <p className="text-xl">{product.price}</p>
           </div>
         </li>
       ))}
