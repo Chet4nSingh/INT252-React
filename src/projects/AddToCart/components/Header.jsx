2eaca5c10dc94d776d6f09e0e17000e92101b89d
Chetan Singh <chet4nsingh@gmail.com>
2024-09-01 21:18:48 +0530

    React + Tailwind CSS

--- src/projects/AddToCart/components/Header.jsx
+++ src/projects/AddToCart/components/Header.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Cart from "./Cart";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
   const [cartIsOpen, setCartIsOpen] = useState(false);
+  const {items} = useContext(CartContext);
 
   return (
     <>
@@ -11,7 +13,7 @@ export default function Header() {
         <h1 className="text-8xl inline">React Shop</h1>
         <div>
           <button onClick={() => setCartIsOpen(true)} className="text-4xl px-4 py-2 pb-3 bg-sky-600 hover:bg-sky-700 rounded-lg">
-            Cart (0)
+            Cart ({items.length})
           </button>
         </div>
       </header>
